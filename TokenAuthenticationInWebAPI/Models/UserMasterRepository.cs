fb93d0199d6d7a5a331119b2d64d1a1297e1bfb4
Alok Kumar Sahoo <alokkumar.sahoo@outlook.com>
2020-04-22 03:31:59 +0530

    Add project files.

---
+++ TokenAuthenticationInWebAPI/Models/UserMasterRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace TokenAuthenticationInWebAPI.Models
+{
+    using System;
+    using System.Linq;
+
+    public class UserMasterRepository : IDisposable
+    {
+        // SECURITY_DBEntities it is your context class
+        readonly SECURITY_DB_Entities context = new SECURITY_DB_Entities();
+        //This method is used to check and validate the user credentials
+        public UserMaster ValidateUser(string username, string password)
+        {
+            return context.UserMasters.FirstOrDefault(user =>
+            user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+            && user.UserPassword == password);
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
